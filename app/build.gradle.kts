f823c3394c640b691ec7fea7e912ba5530677f30
simtun <simonas.tunaitis@gmail.com>
2024-02-10 13:14:09 +0200

    Increase the build version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.boatsbay.weather_stations"
         minSdk = 24
         targetSdk = 34
-        versionCode = 11
-        versionName = "2.1.0"
+        versionCode = 12
+        versionName = "2.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
