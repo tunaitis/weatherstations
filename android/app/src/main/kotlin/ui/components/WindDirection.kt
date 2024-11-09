ce06f72c3b3a5da8e16ce903396420d49591e6be
simtun <simonas.tunaitis@gmail.com>
2024-11-09 13:04:58 +0200

    Fix the wind arrows displayed in the wrong direction

--- android/app/src/main/kotlin/ui/components/WindDirection.kt
+++ android/app/src/main/kotlin/ui/components/WindDirection.kt
@@ -16,13 +16,13 @@ import androidx.compose.ui.Modifier
 @Composable
 fun WindDirection(name: String?, modifier: Modifier = Modifier) {
     when (name?.toLowerCase()) {
-        "šiaurės" -> Icon(Icons.Default.North, contentDescription = null, modifier = modifier)
-        "šiaurės rytų" -> Icon(Icons.Default.NorthEast, contentDescription = null, modifier = modifier)
-        "rytų" -> Icon(Icons.Default.East, contentDescription = null, modifier = modifier)
-        "pietryčių" -> Icon(Icons.Default.SouthEast, contentDescription = null, modifier = modifier)
-        "pietų" -> Icon(Icons.Default.South, contentDescription = null, modifier = modifier)
-        "pietvakarių" -> Icon(Icons.Default.SouthWest, contentDescription = null, modifier = modifier)
-        "vakarų" -> Icon(Icons.Default.West, contentDescription = null, modifier = modifier)
-        "šiaurės vakarų" -> Icon(Icons.Default.NorthWest, contentDescription = null, modifier = modifier)
+        "šiaurės" -> Icon(Icons.Default.South, contentDescription = null, modifier = modifier)
+        "šiaurės rytų" -> Icon(Icons.Default.SouthWest, contentDescription = null, modifier = modifier)
+        "rytų" -> Icon(Icons.Default.West, contentDescription = null, modifier = modifier)
+        "pietryčių" -> Icon(Icons.Default.NorthWest, contentDescription = null, modifier = modifier)
+        "pietų" -> Icon(Icons.Default.North, contentDescription = null, modifier = modifier)
+        "pietvakarių" -> Icon(Icons.Default.NorthEast, contentDescription = null, modifier = modifier)
+        "vakarų" -> Icon(Icons.Default.East, contentDescription = null, modifier = modifier)
+        "šiaurės vakarų" -> Icon(Icons.Default.SouthEast, contentDescription = null, modifier = modifier)
     }
 }
